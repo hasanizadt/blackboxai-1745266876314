7611b3cd19b49f6c0c8b21c6c0c29027b883e84e
hasanizadt <77549972+hasanizadt@users.noreply.github.com>
2025-04-21 23:51:41 +0330

    Add frontend/main/admin.jsx 1745266901018

---
+++ frontend/main/admin.jsx
@@ -0,0 +1,8 @@
+export default function AdminPanel() {
+  return (
+    <main className="min-h-screen p-4">
+      <h1 className="text-3xl font-bold mb-4">پنل مدیریت</h1>
+      <p>مدیریت سایت از اینجا انجام می‌شود.</p>
+    </main>
+  );
+}
