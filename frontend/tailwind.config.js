353f3ab70fc21982e567f0fdd947a8422ba978d0
hasanizadt <77549972+hasanizadt@users.noreply.github.com>
2025-04-21 23:51:56 +0330

    Add frontend/tailwind.config.js 1745266915469

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx}",
+    "./client/**/*.{js,jsx}",
+    "./seller/**/*.{js,jsx}",
+    "./main/**/*.{js,jsx}"
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
